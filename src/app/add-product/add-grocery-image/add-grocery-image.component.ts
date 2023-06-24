3586ed21ab61fd13e4419c007d0f1bd3770ffcf7
Thomas Sarfo <tomsarfodavis@gmail.com>
2023-06-24 21:09:06 +0000

    Re-commiting two years ago file

---
+++ src/app/add-product/add-grocery-image/add-grocery-image.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-grocery-image',
+  templateUrl: './add-grocery-image.component.html',
+  styleUrls: ['./add-grocery-image.component.css']
+})
+export class AddGroceryImageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
